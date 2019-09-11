88fb90fae0475dad5f69783f6c582ef5e0736638
jhonatanmarte <jmarte@gmail.com>
2019-09-11 19:18:27 -0400

    primer commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+app.use(express.static(__dirname + '/public'));
+
+// const foot: {
+//     nombre: 'Jhonatan',
+//     anio: new Date().getFullYear()
+
+// }
+
+const port = process.env.port || 3000;
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Jhonatan',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+
+    });
+});
+
+// app.get('/about', (req, res) => {
+//     res.render('about', foot);
+// });
+
+
+// app.get('/data', function(req, res) {
+//     res.send('Hola Data')
+
+// });
+app.listen(port, () => {
+    console.log(`Escuchando en puerto ${port}`);
+});
