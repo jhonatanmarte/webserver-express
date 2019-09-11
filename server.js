a2d9c794faa46a2825db9d456536fc2de08f8606
jhonatanmarte <jmarte@gmail.com>
2019-09-11 19:34:26 -0400

    typpo

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'));
 
 // }
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //Express HBS engine
 hbs.registerPartials(__dirname + '/views/parciales');
